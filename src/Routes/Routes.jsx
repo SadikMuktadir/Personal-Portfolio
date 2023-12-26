3e91576d51a9802ef675640f2dee551d812ff473
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-26 10:28:57 +0600

    Navbar and footer section created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../components/MainLayout";
+import Home from "../components/Home";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      }
+    ]
   },
 ]);
 const Routes = () => {
   return (
-    <div>
+    <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router} />
     </div>
   );
