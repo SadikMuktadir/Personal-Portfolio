4c9f1ce5f1fde0939be7e85498e35cc765753229
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-31 14:30:59 +0600

    AOS applied

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,30 @@
+import { useEffect } from "react";
 import Banner from "./Banner";
 import MyExperience from "./MyExperience";
 import MyServices from "./MyServices";
 import WhyHire from "./WhyHire";
-
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <MyServices></MyServices>
-            <MyExperience></MyExperience>
-            <WhyHire></WhyHire>
-        </div>
-    );
+  useEffect(() => {
+    AOS.init({ duration: "1000" });
+  }, []);
+  return (
+    <div>
+      <div data-aos="zoom-in">
+        <Banner></Banner>
+      </div>
+      <div data-aos="zoom-in">
+        <MyServices></MyServices>
+      </div>
+      <div data-aos="zoom-in">
+        <MyExperience></MyExperience>
+      </div>
+      <div data-aos="zoom-in">
+        <WhyHire></WhyHire>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
